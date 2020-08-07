3bc7ff8e1c4462e4610c7202168bd278e8f9bc77
Josh <josh.ja.butt@gmail.com>
2020-08-08 00:57:13 +1000

    Add guild wide markov

--- cogs/markov.py
+++ cogs/markov.py
@@ -11,10 +11,13 @@ from bot import BotBase, Context
 from cogs.logging import is_public
 
 
-async def get_message_log(user: discord.User, conn: asyncpg.Connection) -> str:
+async def get_user_message_log(user: discord.User, conn: asyncpg.Connection) -> str:
     record = await conn.fetchrow('SELECT string_agg(content, \'\n\') AS data FROM logging.message_log WHERE user_id = $1', user.id)
     return record['data']
 
+async def get_guild_message_log(guild: discord.Guild, conn: asyncpg.Connection) -> str:
+    record = await conn.fetchrow('SELECT string_agg(content, \'\n\') AS data FROM logging.message_log WHERE guild_id = $1', guild.id)
+    return record['data']
 
 def get_markov(data: str, *, state_size: int = 2, seed: str = None) -> Optional[str]:
     model = markovify.NewlineText(input_text=data, state_size=state_size)
@@ -42,7 +45,7 @@ class Markov(commands.Cog):
 
         async with ctx.db as conn:
             await is_public(ctx, user, conn)
-            data = await get_message_log(user, conn)
+            data = await get_user_message_log(user, conn)
 
             if not data:
                 raise commands.BadArgument(f'User "{user}" currently has no message log data, please try again later.')
@@ -52,6 +55,22 @@ class Markov(commands.Cog):
             markov = await self.bot.loop.run_in_executor(None, markov_call) or '> Markov could not be generated.'
             await ctx.send(markov)
 
+
+    @commands.command(name='guild_markov', aliases=['gm'])
+    @commands.guild_only()
+    async def user_markov(self, ctx: Context):
+        """Generate a markov chain based off messages in the server.
+        """
+        async with ctx.db as conn:
+            data = await get_guild_message_log(ctx.guild, conn)
+
+            if not data:
+                raise commands.BadArgument(f'Server "{guild.name}" currently has no message log data, please try again later.')
+
+        async with ctx.typing():
+            markov_call = partial(get_markov, data)
+            markov = await self.bot.loop.run_in_executor(None, markov_call) or '> Markov could not be generated.'
+            await ctx.send(markov)
 
 def setup(bot: BotBase):
     bot.add_cog(Markov(bot))
