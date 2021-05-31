f6d601e120e46ee3a1151b038b461f04d9181b96
Josh <josh.ja.butt@gmail.com>
2021-05-31 19:19:07 +1000

    Add mode switcher

--- cogs/games/voltorb_flip.py
+++ cogs/games/voltorb_flip.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import io
+from typing import Optional
 from PIL import Image
 
 import discord
@@ -10,6 +11,41 @@ from ditto import CONFIG, BotBase, Cog, Context
 from ditto.types import User
 
 COG_CONFIG = CONFIG.EXTENSIONS[__name__]
+
+
+class ModeButton(discord.ui.Button["Mode"]):
+    def __init__(self, value: Optional[vflip.types.Value]):
+        self.value: Optional[vflip.types.Value] = value
+
+        if value is None:
+            style = discord.ButtonStyle.danger
+            disabled = True
+            label = "Flip!"
+        else:
+            style = discord.ButtonStyle.primary
+            disabled = False
+            label = str(value)
+
+        super().__init__(style=style, label=label, disabled=disabled)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        self.view.mode = self.value
+
+
+MODE_BUTTONS = []
+
+
+class Mode(discord.ui.View):
+    children: list[ModeButton]
+
+    def __init__(self, game: Game):
+        super().__init__(timeout=None)
+        self.game = game
+        self.mode: Optional[vflip.types.Value] = None
+
+        for button in MODE_BUTTONS:
+            self.add_item(button)
 
 
 class Button(discord.ui.Button["Game"]):
@@ -23,7 +59,10 @@ class Button(discord.ui.Button["Game"]):
         return self.view.game[self.row, self.col]
 
     async def callback(self, interaction: discord.Interaction):
-        self.square.flip()
+        if self.view.mode is None:
+            self.square.flip()
+        else:
+            self.square.note(self.view.mode)
 
         self.disabled = True
 
@@ -57,12 +96,17 @@ class Game(discord.ui.View):
         self.player = player
         self.game = vflip.Board(level)  # type: ignore
         self.last_messsage = None
+        self.mode_switcher = Mode(self)
 
         super().__init__(timeout=None)
 
         for row in range(5):
             for col in range(5):
                 self.add_item(Button(row, col))
+
+    @property
+    def mode(self) -> Optional[vflip.types.Value]:
+        return self.mode_switcher.mode
 
     async def interaction_check(self, interaction: discord.Interaction) -> bool:
         if interaction.user != self.player:
@@ -82,6 +126,22 @@ class Game(discord.ui.View):
         self.last_messsage = await COG_CONFIG.RENDER_CHANNEL.send(file=discord.File(fp, "voltorb.png"))
         return self.last_messsage.attachments[0].url
 
+    def stop(self) -> None:
+
+        for button in self.mode_switcher.children:
+            button.disabled = True
+
+        self.mode_switcher.stop()
+        super().stop()
+
+    @classmethod
+    async def setup(cls, ctx: Context, level: int):
+        game = cls(ctx.author, level)
+        embed = game.embed.set_image(url=await game.render())
+        await ctx.send(embed=embed, view=game)
+        await ctx.send(content="\u200b", view=game.mode_switcher)
+        return game
+
     @property
     def embed(self) -> discord.Embed:
         return discord.Embed()
@@ -100,9 +160,7 @@ class VoltorbFlip(Cog):
         # if ctx.author.id in self.games:
         #     raise commands.BadArgument("you are already playing.")
 
-        game = Game(ctx.author, level)
-        embed = game.embed.set_image(url=await game.render())
-        await ctx.send(embed=embed, view=game)
+        await Game.setup(ctx, level)
 
 
 def setup(bot: BotBase):
